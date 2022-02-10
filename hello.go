b04c39c1080cdfca77345a95cd1d7bdd44b1cd24
shivanithemathlete <shivanithemathlete20@gmail.com>
2022-02-10 15:15:59 +0530

    first change

--- hello.go
+++ hello.go
@@ -1,5 +1,9 @@
 package main
+
 import "fmt"
-func main(){
-    fmt.Println("Hello, world.")
+
+func main() {
+	fmt.Println("Hello, world.")
+	fmt.Printf("Checking update on git\n")
+	fmt.Printf("Changes reflected\n")
 }
